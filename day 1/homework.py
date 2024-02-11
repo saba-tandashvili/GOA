67cde72c5f25e21122358a7cd45bf721b88197ed
saba-tandashvili <sb.tandashvili@gmail.com>
2024-02-11 12:00:23 +0400

    commit

---
+++ day 1/homework.py
@@ -0,0 +1,7 @@
+name = "Saba" 
+last_name = "Tandashvili" 
+age = 13 
+height = 160 
+weight = 45
+
+print("My full name is " + name + " " + last_name + " i am " + str(age) + " years old, my height is " + str(height) + "cm, also my weight is " + str(weight) + "kg.")
---
+++ day 2/homework.py
@@ -0,0 +1,3 @@
+print("me radgan axali shemisuli var jer bevrs aravis davumegobrdi, erts davumegobrdi razmis liders")
+
+print("GOAze axali ki var magram ukve shemiyvarda awavtoba. bavshebit gadatenilia da imedia bevrs gavicnob da davimegobreb")
